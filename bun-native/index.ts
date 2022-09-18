266a4eeda5b90fe67ce926af25c1130d12a763fd
Imam Fahrur Rofi <imam@fahru.my.id>
2022-09-18 07:43:59 +0700

    Moving Headers

--- bun-native/index.ts
+++ bun-native/index.ts
@@ -1,23 +1,25 @@
-const headers = new Headers({
-  'Content-Type': 'application/json; charset=UTF-8',
-  'Connection': 'Keep-Alive',
-  'Keep-Alive': 'timeout=72',
-});
-
 Bun.serve({
   async fetch(req: Request) {
     switch (req.method) {
       case 'GET':
-        return new Response(JSON.stringify({ status: "OK" }), {
+        return new Response(JSON.stringify({ status: 'OK' }), {
           status: 200,
-          headers
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            Connection: 'Keep-Alive',
+            'Keep-Alive': 'timeout=72',
+          },
         });
         break;
       case 'POST':
-        const data = await req.json()
+        const data = await req.json();
         return new Response(JSON.stringify(data), {
           status: 200,
-          headers
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            Connection: 'Keep-Alive',
+            'Keep-Alive': 'timeout=72',
+          },
         });
         break;
       default:
--- deno-native/index.ts
+++ deno-native/index.ts
@@ -1,24 +1,26 @@
 import { serve } from 'https://deno.land/std/http/server.ts';
 
-const headers = new Headers({
-  'Content-Type': 'application/json; charset=UTF-8',
-  'Connection': 'Keep-Alive',
-  'Keep-Alive': 'timeout=72',
-});
-
 async function handler(req: Request) {
   switch (req.method) {
     case 'GET':
-      return new Response(JSON.stringify({ status: "OK" }), {
+      return new Response(JSON.stringify({ status: 'OK' }), {
         status: 200,
-        headers
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          Connection: 'Keep-Alive',
+          'Keep-Alive': 'timeout=72',
+        },
       });
       break;
     case 'POST':
-      const data = await req.json()
+      const data = await req.json();
       return new Response(JSON.stringify(data), {
         status: 200,
-        headers
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          Connection: 'Keep-Alive',
+          'Keep-Alive': 'timeout=72',
+        },
       });
       break;
     default:
