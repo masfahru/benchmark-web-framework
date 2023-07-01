b0c33b63964199e2ef342c88627b64480a183f6f
Imam Fahrur Rofi <imamfahrurrofi@gmail.com>
2023-07-01 09:50:44 +0700

    feat: add nestjs - express

---
+++ nest-express/src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get, Post, Req } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  @Get()
+  getHello() {
+    return {
+      status: 'OK',
+    };
+  }
+
+  @Post()
+  postHello(@Req() request: any) {
+    return request.body;
+  }
+}
